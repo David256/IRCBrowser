9e59f968199164ac944bb5143cde7bbb0fae5201
David256 <davidborja456@gmail.com>
2015-10-31 00:41:42 -0500

    Escribiendo el IRCBrowser

---
+++ IRCBrowser.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+
+#
+# IRCBrowser:
+#
+# Creado por David256 para la comunidad HIRA y/o IRC, pero por el momento
+# se esta desarrollando. Hey otro-usuario, puedes aprender con mi código
+# sin problema, es libre, pero.. no es ético decir que es tuyo cuando no
+# lo es.
+# Da los respectivos crédito y se felíz. Que tengas un buen <3  día  <3
+#
+# https://github.com/David256
+
+
+
+import omg.IRC as IRC
+import omg.Browser as Browser
+
+#creacion de objetos necesarios
+
+irc = IRC.irc()
+browser = Browser.browser()
