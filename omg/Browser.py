e1b879c79420af3120c39fcb5820f2b062c6a2ff
David256 <davidborja456@gmail.com>
2015-10-31 00:32:29 -0500

    Iniciando clases Browser

---
+++ omg/Browser.py
@@ -0,0 +1,17 @@
+import socket
+
+class browser:
+
+	def __init__(self):
+		#iniciamos variables
+		self.url = ""
+		self.contenido = ""
+		self.estado = 000
+
+	def corregirUrl(self, url):
+		#vamos a corregir las url y obtener los datos necesarios
+		pass
+
+	def hacerPeticion(self, host, path, parametros):
+		#vamos a hacer la peticion y a devolver el valor obtenido
+		pass
